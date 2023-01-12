e326054acf487ca3f5deb42c8a7a80998266ade9
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-12 12:32:06 +0300

    Restructuring my project

--- app.py
+++ app.py
@@ -7,11 +7,3 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
 
-@app.route("/")
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
-@app.route('/market')
-def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
