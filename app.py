1289ac50e26a83bb52c2f3d1999f171fe051b370
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-12 11:56:30 +0300

    Added a database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 
@@ -13,7 +14,7 @@ class Item(db.Model):
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
  
     def __repr__(self):
-        return f'Item{self.name}'
+        return f'Item {self.name}'
  
  
 @app.route("/")
@@ -22,9 +23,5 @@ def home_page():
     return render_template('home.html')
 @app.route('/market')
 def market_page():
-    items = [
-         {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-         {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-         {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-]
+    items = Item.query.all()
     return render_template('market.html', items=items)
