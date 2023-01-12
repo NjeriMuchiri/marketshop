139f0da63d2011e4f64720455f7c5f2d9a601f07
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-12 12:30:45 +0300

    Restructuring my project

--- app.py
+++ app.py
@@ -6,17 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 
 db = SQLAlchemy(app)
 
-class Item(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False,unique=True)
-    price = db.Column(db.Integer())
-    barcode = db.Column(db.String(length=12), nullable=False,unique=True)
-    description = db.Column(db.String(length=1024), nullable=False, unique=True)
- 
-    def __repr__(self):
-        return f'Item {self.name}'
- 
- 
+
 @app.route("/")
 @app.route('/home')
 def home_page():
