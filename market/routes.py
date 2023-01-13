1c234f2cc24c49c5cb634c7f24c59eb4f3acce32
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-13 12:46:52 +0300

    validating form file

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
 from flask import render_template
-from market.models import Item
+from market.models import Item,User
 from market.forms  import RegisterForm
+from market import db
 
 @app.route("/")
 @app.route('/home')
@@ -15,4 +16,10 @@ def market_page():
 @app.route('/register')
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password.data)
+        db.session.add(user_to_create)
+        db.session.commit()
     return render_template('register.html', form=form)
