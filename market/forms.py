8f4430ca617fbdc5a7b0564e54f9f283225d0d57
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-14 10:29:26 +0300

    Hashing passwords with bcrypt

--- market/forms.py
+++ market/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Length,EqualTo,Email,InputRequired
-
+from wtforms.validators import Length,EqualTo,Email,InputRequired,ValidationError
+from market.models import User
 class RegisterForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists! Please try a different username')
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email address already exists! Please try a different email')
     username = StringField(label='User Name', validators=[Length(min=2, max=30),InputRequired()])
     email_address = StringField(label='Email Address', validators=[Email(),InputRequired()])
     password1 = PasswordField(label='Password', validators=[Length(min=6),InputRequired()])
