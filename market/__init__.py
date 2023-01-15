f7cc450a6a1dffe8aa6e45259d185e1bbe1ca1c8
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-15 16:44:20 +0300

    Adding a new navbar yes

--- market/__init__.py
+++ market/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_fontawesome import FontAwesome
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'ca5ceda1d0fe49f2f384944f'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+fa = FontAwesome(app)
 login_manager = LoginManager(app)
 from market import routes
